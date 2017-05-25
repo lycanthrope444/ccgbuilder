069109a55f99bd2ac2a34944af8a37562f2e747c
Nathan <nathanstarwalt@gmail.com>
2017-05-25 17:48:06 -0400

    rough sketch of deck display

--- app/scripts/components/deckbuilder.jsx
+++ app/scripts/components/deckbuilder.jsx
@@ -9,12 +9,28 @@ class DeckbuilderContainer extends React.Component {
 }
 
 class SearchControl extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      decklist : {
+        deckName:'Deck Name',
+        cardList: [
+          {cardName: 'test 1', qty: 1},
+          {cardName: 'test 2', qty: 1}
+        ]
+      },
+      searchResults : ['test']
+    }
+  }
   render(){
+    var self = this;
+
     return (
       <div>
         <SearchOptions />
-        <SearchResults />
-        <Decklist />
+        <SearchResults searchResults = {self.state.searchResults} />
+        <Decklist decklist = {self.state.decklist} />
       </div>
     )
   }
@@ -31,7 +47,11 @@ class SearchOptions extends React.Component {
 }
 
 class SearchResults extends React.Component {
+  constructor(props){
+    super(props);
+  }
   render(){
+    console.log(this.props);
     return (
       <div className="search-results col-xs-6">
         Results
@@ -41,10 +61,40 @@ class SearchResults extends React.Component {
 }
 
 class Decklist extends React.Component {
+  constructor(props){
+    super(props);
+    var decklist = this.props.decklist;
+
+
+
+    this.state = {
+      deckName:decklist.deckName,
+      cardList:decklist.cardList
+    }
+  }
   render(){
+    console.log(this.state);
+
+    var cardDisplay = this.state.cardList.map((item,index)=>{
+      return (
+        <div key={index}>
+          {item.cardName}
+        </div>
+      )
+    });
+
     return(
       <div className="decklist col-xs-6">
         Deck List
+        <div className = "name-area">
+          <h1>{this.state.deckName}</h1>
+          <button className ="btn btn-primary">
+            Change Name
+          </button>
+        </div>
+        <div className = "cardList">
+          {cardDisplay}
+        </div>
       </div>
     )
   }
